05ccf5da9fd1baae02c548ef6d8993874dbaa3fb
JavEllis <J.ELLIS-2022@hull.ac.uk>
2024-12-12 05:41:06 +0000

    added pieces of code that allows users  to select what role they are along with assigning passcodes for tutors and supervisors

--- DDDprogramCode/Program.cs
+++ DDDprogramCode/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    //sets passcodes for the ST and PT so only them can access there part 
+    const string PS_PASSCODE = "12345";
+    const string ST_PASSCODE = "67890";
+    const string SUPERVISOR_NAME = "Nia Lee";
+    const string TUTOR_NAME = "Andy Grey";
+
+    static void Main(string[] args)
+    {
+        //asks user to select the role they what to access
+        Console.WriteLine("Welcome to our program, please select what the following applies to you:");
+        Console.WriteLine("1. Student");
+        Console.WriteLine("2. Personal Supervisor (PS)");
+        Console.WriteLine("3. Senior Tutor (ST)");
+        string role = Console.ReadLine();
